37d9591d1505d1df72b3f0566a23c127e66e3dc1
choi minuk <richard@ifunfactory.com>
2020-06-08 22:24:43 +0900

    Updates

---
+++ precache-manifest.9043f0a50034699ab3add9524a366001.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8eec9b44a52f7cbf850bd7b746da75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "216671a7145b6904b04b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86bf387d40991f19d023",
+    "url": "/static/js/2.c53ae97c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c53ae97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216671a7145b6904b04b",
+    "url": "/static/js/main.67232510.chunk.js"
+  },
+  {
+    "revision": "67d32dc3bb54fa3690c1",
+    "url": "/static/js/runtime-main.450ce6df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
